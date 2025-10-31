d97e667e2d2cf869704aa95c03de3bbbc55e956f
Luiz Togni <contato.togni@gmail.com>
2025-10-31 15:07:24 -0300

    test: testing either unit

---
+++ src/core/either.spec.ts
@@ -0,0 +1,23 @@
+import { fail, success, type Either } from "./either.js";
+
+const doSomething = (shouldSuccess: boolean): Either<string, string> => {
+  if (shouldSuccess) {
+    return success("success");
+  }
+
+  return fail("error");
+};
+
+test("success result", () => {
+  const result = doSomething(true);
+
+  expect(result.isFailed()).toBe(false);
+  expect(result.isSuccessed()).toBe(true);
+});
+
+test("failure result", () => {
+  const result = doSomething(false);
+
+  expect(result.isFailed()).toBe(true);
+  expect(result.isSuccessed()).toBe(false);
+});
