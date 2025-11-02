a73f51fb30982edca27dc0752d40a59283d01271
Luiz Togni <contato.togni@gmail.com>
2025-11-01 21:52:01 -0300

    feat: add findMany in categories repository interface

--- src/domain/shopping-list/application/repositories/categories-repository.ts
+++ src/domain/shopping-list/application/repositories/categories-repository.ts
@@ -1,5 +1,7 @@
+import type { IPaginationParams } from "#core/repositories/pagination-params.js";
 import { Category } from "#domain/shopping-list/enterprise/entities/category.js";
 
 export interface ICategoriesRepository {
   create(category: Category): Promise<void>;
+  findMany(params: IPaginationParams): Promise<Category[]>;
 }
