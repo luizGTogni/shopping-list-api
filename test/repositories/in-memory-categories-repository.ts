d2e61de06114e90dcd27ad0466c1deff6ac038ec
Luiz Togni <contato.togni@gmail.com>
2025-11-01 21:53:10 -0300

    feat: add findMany implementations in in memory categories repository

--- test/repositories/in-memory-categories-repository.ts
+++ test/repositories/in-memory-categories-repository.ts
@@ -1,10 +1,19 @@
+import type { IPaginationParams } from "#core/repositories/pagination-params.js";
 import { ICategoriesRepository } from "#domain/shopping-list/application/repositories/categories-repository.js";
 import { Category } from "#domain/shopping-list/enterprise/entities/category.js";
 
 export class InMemoryCategoriesRepository implements ICategoriesRepository {
+  private NUMBER_BY_PAGE = 20;
   public items: Category[] = [];
 
   async create(category: Category): Promise<void> {
     this.items.push(category);
+  }
+
+  async findMany({ page }: IPaginationParams): Promise<Category[]> {
+    const startIndex = (page - 1) * this.NUMBER_BY_PAGE;
+    const endIndex = page * this.NUMBER_BY_PAGE;
+
+    return this.items.sort((a, b) => a.name.localeCompare(b.name)).slice(startIndex, endIndex);
   }
 }
