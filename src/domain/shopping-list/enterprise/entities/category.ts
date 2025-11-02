14b4126c4f4b945cc3c6337cb695efe497f17eca
Luiz Togni <contato.togni@gmail.com>
2025-11-02 00:56:01 -0300

    feat: add deletedAt property and remove method in category entity

--- src/domain/shopping-list/enterprise/entities/category.ts
+++ src/domain/shopping-list/enterprise/entities/category.ts
@@ -4,6 +4,7 @@ import type { UniqueEntityID } from "#core/entities/unique-entity-id.js";
 export interface ICategoryProps {
   name: string;
   description: string;
+  deletedAt?: Date;
 }
 
 export class Category extends Entity<ICategoryProps> {
@@ -15,12 +16,20 @@ export class Category extends Entity<ICategoryProps> {
     return this.props.description;
   }
 
+  get deletedAt() {
+    return this.props.deletedAt;
+  }
+
   set name(name: string) {
     this.props.name = name;
   }
 
   set description(description: string) {
     this.props.description = description;
+  }
+
+  remove() {
+    this.props.deletedAt = new Date();
   }
 
   static create(props: ICategoryProps, id?: UniqueEntityID) {
