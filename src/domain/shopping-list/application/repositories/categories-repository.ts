98db575d9182b2edffe4d4ceb4f3bcf3b122347b
Luiz Togni <contato.togni@gmail.com>
2025-11-02 00:21:57 -0300

    feat: add findById in categories repository interface

--- src/domain/shopping-list/application/repositories/categories-repository.ts
+++ src/domain/shopping-list/application/repositories/categories-repository.ts
@@ -3,5 +3,6 @@ import { Category } from "#domain/shopping-list/enterprise/entities/category.js"
 
 export interface ICategoriesRepository {
   create(category: Category): Promise<void>;
+  findById(id: string): Promise<Category | null>;
   findMany(params: IPaginationParams): Promise<Category[]>;
 }
