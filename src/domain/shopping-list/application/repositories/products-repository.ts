2c31e47e4fc828497c1e3a95b3acd4b06e7081d0
Luiz Togni <contato.togni@gmail.com>
2025-11-02 21:33:31 -0300

    feat: add findMany in products repository interface

--- src/domain/shopping-list/application/repositories/products-repository.ts
+++ src/domain/shopping-list/application/repositories/products-repository.ts
@@ -1,5 +1,7 @@
+import type { IPaginationParams } from "#core/repositories/pagination-params.js";
 import { Product } from "#domain/shopping-list/enterprise/entities/product.js";
 
 export interface IProductsRepository {
   create(product: Product): Promise<void>;
+  findMany(params: IPaginationParams): Promise<Product[]>;
 }
