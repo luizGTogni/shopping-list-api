bc63341365ee7e633a3e0586bf6f4dc1f9472b79
Luiz Togni <contato.togni@gmail.com>
2025-11-03 16:39:37 -0300

    feat: add create shopping list use case

---
+++ src/domain/shopping-list/application/use-cases/shopping-list/create-shopping-list.ts
@@ -0,0 +1,40 @@
+import { Either, fail, success } from "#core/either.js";
+import { UniqueEntityID } from "#core/entities/unique-entity-id.js";
+import { ShopperNotFoundError } from "#core/errors/types/shopper-not-found-error.js";
+import { ShoppingList } from "#domain/shopping-list/enterprise/entities/shopping-list.js";
+import { IShoppersRepository } from "../../repositories/shoppers-repository";
+import { IShoppingListsRepository } from "../../repositories/shopping-lists-repository";
+
+interface ICreateShoppingListUseCaseRequest {
+  shopperId: string;
+  title: string;
+}
+
+type ICreateShoppingListUseCaseResponse = Either<
+  ShopperNotFoundError,
+  { shoppingList: ShoppingList }
+>;
+
+export class CreateShoppingListUseCase {
+  constructor(
+    private readonly shoppingListsRepository: IShoppingListsRepository,
+    private readonly shoppersRepositories: IShoppersRepository,
+  ) {}
+
+  async execute({
+    shopperId,
+    title,
+  }: ICreateShoppingListUseCaseRequest): Promise<ICreateShoppingListUseCaseResponse> {
+    const shopper = await this.shoppersRepositories.findById(shopperId);
+
+    if (!shopper) {
+      return fail(new ShopperNotFoundError());
+    }
+
+    const shoppingList = ShoppingList.create({ shopperId: new UniqueEntityID(shopperId), title });
+
+    await this.shoppingListsRepository.create(shoppingList);
+
+    return success({ shoppingList });
+  }
+}
