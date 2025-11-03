76618c0d0bbde2c56944dbb8b9a8c5e0c9b6db8d
Luiz Togni <contato.togni@gmail.com>
2025-11-03 16:54:02 -0300

    feat: add findManyByShopperId implementation in in memory shopping lists repository

--- src/domain/shopping-list/application/repositories/shopping-lists-repository.ts
+++ src/domain/shopping-list/application/repositories/shopping-lists-repository.ts
@@ -1,5 +1,7 @@
+import { IPaginationParams } from "#core/repositories/pagination-params.js";
 import { ShoppingList } from "#domain/shopping-list/enterprise/entities/shopping-list.js";
 
 export interface IShoppingListsRepository {
   create(shoppingList: ShoppingList): Promise<void>;
+  findManyByShopperId(shopperId: string, params: IPaginationParams): Promise<ShoppingList[]>;
 }
