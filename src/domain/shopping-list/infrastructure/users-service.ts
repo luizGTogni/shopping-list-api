5182d5fd4744d9a53a511e2e17260bbe5e10335a
Luiz Togni <contato.togni@gmail.com>
2025-11-05 22:27:51 -0300

    feat: add users service implementation

---
+++ src/domain/shopping-list/infrastructure/users-service.ts
@@ -0,0 +1,14 @@
+import type { IUsersRepository } from "#domain/users/application/repositories/users-repository.js";
+import type { IUsersService } from "../application/services/users-service-interface";
+
+export class UsersService implements IUsersService {
+  constructor(private readonly usersRepository: IUsersRepository) {}
+
+  async exists(id: string): Promise<boolean> {
+    const user = await this.usersRepository.findById(id);
+
+    if (!user) return false;
+
+    return true;
+  }
+}
