dbbfc2913d3b0e335eaedc72e5a3e84b39d5bc65
Luiz Togni <contato.togni@gmail.com>
2025-11-06 16:44:46 -0300

    refactor: add password in make shopper

--- test/factories/make-shopper.ts
+++ test/factories/make-shopper.ts
@@ -1,13 +1,18 @@
 import { UniqueEntityID } from "#core/entities/unique-entity-id.js";
 import { UserImage } from "#domain/users/enterprise/entities/user-image.js";
 import { IUserProps, User } from "#domain/users/enterprise/entities/user.js";
+import { Password } from "#domain/users/enterprise/entities/value-objects/password.js";
 import { faker } from "@faker-js/faker";
 
 export function makeShopper(override: Partial<IUserProps> = {}, id?: UniqueEntityID) {
+  const password = new Password();
+  password.toHash("123456");
+
   const shopper = User.create(
     {
       name: faker.person.firstName(),
       email: faker.internet.email(),
+      password,
       role: "SHOPPER",
       profileImage: UserImage.create({
         userId: new UniqueEntityID(),
