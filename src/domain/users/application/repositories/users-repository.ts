3017a2f948b309a9e1704a1dd27dc9dce55a3ab1
Luiz Togni <contato.togni@gmail.com>
2025-11-06 15:19:59 -0300

    feat: add findByEmail in users repository

--- src/domain/users/application/repositories/users-repository.ts
+++ src/domain/users/application/repositories/users-repository.ts
@@ -3,4 +3,5 @@ import { User } from "#domain/users/enterprise/entities/user.js";
 export interface IUsersRepository {
   create(user: User): Promise<void>;
   findById(id: string): Promise<User | null>;
+  findByEmail(email: string): Promise<User | null>;
 }
