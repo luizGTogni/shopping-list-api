48313e3b633595d321294b546ea1d19f08b3597d
Luiz Togni <contato.togni@gmail.com>
2025-11-06 16:45:16 -0300

    feat: add generate password hash

--- src/domain/users/application/use-cases/shoppers/create-shopper.ts
+++ src/domain/users/application/use-cases/shoppers/create-shopper.ts
@@ -1,26 +1,31 @@
+import { IHasherDriver } from "#core/drivers/hasher-driver-interface.js";
 import { Either, fail, success } from "#core/either.js";
 import { UniqueEntityID } from "#core/entities/unique-entity-id.js";
 import { UserAlreadyExistsError } from "#core/errors/types/user-already-exists-error.js";
 import { UserImage } from "#domain/users/enterprise/entities/user-image.js";
 import { User } from "#domain/users/enterprise/entities/user.js";
+import { Password } from "#domain/users/enterprise/entities/value-objects/password.js";
 import { IUsersRepository } from "../../repositories/users-repository";
 
 interface ICreateShopperUseCaseRequest {
   name: string;
   email: string;
-  password: string;
+  passwordPlain: string;
   profileImageId: string;
 }
 
 type ICreateShopperUseCaseResponse = Either<UserAlreadyExistsError, { shopper: User }>;
 
 export class CreateShopperUseCase {
-  constructor(private readonly usersRepository: IUsersRepository) {}
+  constructor(
+    private readonly usersRepository: IUsersRepository,
+    private readonly hasherDriver: IHasherDriver,
+  ) {}
 
   async execute({
     name,
     email,
-    password,
+    passwordPlain,
     profileImageId,
   }: ICreateShopperUseCaseRequest): Promise<ICreateShopperUseCaseResponse> {
     const shopperAlreadyExists = await this.usersRepository.findByEmail(email);
@@ -28,6 +33,9 @@ export class CreateShopperUseCase {
     if (!shopperAlreadyExists) {
       return fail(new UserAlreadyExistsError());
     }
+
+    const password = new Password(this.hasherDriver);
+    await password.toHash(passwordPlain);
 
     const shopper = User.create({ name, email, password, role: "SHOPPER" });
 
